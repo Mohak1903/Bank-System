48cd07b80ff70f6d839edaa8dfbaf06998a1749e
Mohak1903 <91303392+Mohak1903@users.noreply.github.com>
2021-10-20 22:15:14 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,29 @@
+<div id="container">
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+  <br>
+    <div id="footer">© 2021. Made by Mohak Pal <br>
+      For the Project of The Sparks Foundation</div>
+      <br>
+ </div>
